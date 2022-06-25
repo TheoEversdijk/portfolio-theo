204837cbcc4da016d4fea8e34bbf107dcfb8a596
Theo <ever0045@hz.nl>
2022-06-25 17:41:29 +0200

    Breeze

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Blog\BlogController;
-use App\Http\Controllers\Blog\DamenController;
-use App\Http\Controllers\Blog\ErvaringController;
-use App\Http\Controllers\Blog\FeedbackController;
-use App\Http\Controllers\Blog\StudiekeuzeController;
-use App\Http\Controllers\Blog\SwotController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\FaqsController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,21 +14,16 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
-Route::get('/', [WelcomeController::class, 'show']);
-Route::get('/faq', [FaqsController::class, 'show']);
-Route::get('/profile', [ProfileController::class, 'show']);
-Route::get('/blog', [BlogController::class, 'show']);
 Route::get('/dashboards', [ContactController::class, 'index'])->middleware(['auth']);
-Route::get('/blog/damen', [DamenController::class, 'show']);
-Route::get('/blog/ervaring', [ErvaringController::class, 'show']);
-Route::get('/blog/feedback', [FeedbackController::class, 'show']);
-Route::get('/blog/studiekeuze', [StudiekeuzeController::class, 'show']);
-Route::get('/blog/swot', [SwotController::class, 'show']);
 
 Route::post('/contact', [ContactController::class, 'create']);
