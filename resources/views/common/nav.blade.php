9299a048ae662ea8199598e155d10247057df26d
Theo <ever0045@hz.nl>
2022-07-06 04:18:39 +0200

    Update nav.blade.php

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -1,1 +1,22 @@
-<nav></nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <i class="fa fa-home" aria-hidden="true"></i><a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <i class="fa fa-tachometer" aria-hidden="true"></i<a class="nav-link" href="#">Voortgang</a>
+            </li>
+            <li class="nav-item">
+                <i class="fa fa-folder-open" aria-hidden="true"></i><a class="nav-link" href="#">Projecten</a>
+            </li>
+            <li class="nav-item">
+                <i class="fa fa-users" aria-hidden="true"></i><a class="nav-link disabled" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
