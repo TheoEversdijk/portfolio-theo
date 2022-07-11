722aca27db49e41df0de55aa1ae501a1f6c509cb
Theo <ever0045@hz.nl>
2022-07-11 20:22:43 +0200

    Removed proxy

--- public/controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("./proxy.php");
-
-//setup the Proxy with the right credentials
-$mySwapiProxy = new Proxy('https://api.kanye.rest');
-//get the data
-$result = $mySwapiProxy->getData('people/1');
-//print the data to the frontend.
-print_r($result);
